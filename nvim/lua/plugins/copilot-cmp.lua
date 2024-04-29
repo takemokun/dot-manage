206595984f4591e43dc42dd76a8dda27e8354bd3
takemokun <takemo.takemoun@gmail.com>
2024-04-29 20:14:28 +0900

    🚮 del my dotfiles

--- nvim/lua/plugins/copilot-cmp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "zbirenbaum/copilot-cmp",
-  cmd = "Copilot",
-  config = function()
-      require("copilot_cmp").setup()
-  end,
-}
--- nvim/lua/plugins/nvim-lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'neovim/nvim-lspconfig',
-  config = function() 
-      -- require("mason").setup({})
-      -- require("mason-lspconfig").setup()
-      require("supports.lsp.lspconfig")
-  end,
-}
